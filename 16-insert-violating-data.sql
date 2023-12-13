3591634fea7fc8105f77e3ef0c5e9afa32d988a7
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-12-13 18:34:11 +0530

    sql-cmd

---
+++ 16-insert-violating-data.sql
@@ -0,0 +1,4 @@
+-- INSERT INTO users(full_name, yearly_salary, current_status) VALUES('Some User', -1000, 'unemployed')
+
+
+-- new row for relation "users" violates check constraint "yearly_salary_positive"
