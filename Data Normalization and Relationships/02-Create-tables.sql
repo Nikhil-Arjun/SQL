35fe2ed9a77c49782edfedc8cbc70016c596879d
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-12-17 19:01:58 +0530

    Data Normalization and Relationship

---
+++ Data Normalization and Relationships/02-Create-tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Users (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(300) NOT NULL,
+  address_id SERIAL NOT NULL
+);
+
+
+CREATE TABLE addresses(
+  id SERIAL PRIMARY KEY,
+  street VARCHAR(300) NOT NULL,
+  house_number VARCHAR(50) NOT NULL,
+  city_id SERIAL NOT NULL
+);
+
+CREATE TABLE Cities(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(300) NOT NULL
+);
---
+++ Data Normalization and Relationships/03-insert-data.sql
@@ -0,0 +1,20 @@
+-- INSERT INTO cities(name)
+-- VALUES ('Berlin'),('New York'), ('Mumbai'), ('Delhi');
+
+-- INSERT INTO addresses(street, house_number, city_id)
+-- VALUES 
+--   ('Test street', '8A', 3),
+--   ('Some street', '10', 1),
+--   ('Teststreet', 'B-2', 3),
+--   ('My street', '6D', 4),
+--   ('Someone street', '23', 2);
+
+
+-- INSERT INTO Users (first_name, last_name, email, address_id)
+-- VALUES 
+--   ('Max', 'Schwarz', 'max@test.com', 3),
+--   ('Manuel', 'Lorenz', 'manu@test.com', 1),
+--   ('John', 'Wick', 'john@test.com',2),
+--   ('Julie', 'Barnes', 'julie@barnes.com', 3),
+--   ('Pradip', 'Dev', 'pradip@test.com',4);
+
