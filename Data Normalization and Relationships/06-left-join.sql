0242513af026483eae9fa8269adb8047a6412784
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-12-18 12:57:24 +0530

    Filtering Data using LEFT JOIN

---
+++ Data Normalization and Relationships/06-left-join.sql
@@ -0,0 +1,30 @@
+-- SELECT *
+-- FROM users AS u
+-- LEFT JOIN addresses AS a
+-- ON a.id = u.address_id
+
+-- To view tables values content
+-- SELECT * FROM addresses;
+-- SELECT * FROM users;
+-- SELECT * FROM cities;
+
+
+-- Following SQL statement will select all addresses
+
+-- SELECT * 
+-- FROM addresses AS a
+-- LEFT JOIN users AS U
+-- ON a.id = u.address_id;
+
+
+-- USING Combining multiple LEFT JOINs
+
+SELECT a.id, a.street, a.house_number, 
+      u.first_name, u.last_name, u.email,
+      c.name AS city_name
+FROM addresses AS a
+LEFT JOIN Users AS u
+ON a.id = u.address_id
+LEFT JOIN cities AS C
+ON c.id = a.city_id
+ORDER BY address_id;
